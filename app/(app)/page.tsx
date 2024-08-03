c3e5ae8fe0f135993df2494c31d27e63a47b5648
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-04 02:12:22 +0700

    fix: padding 0 is good

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -3,7 +3,7 @@ import { HeroSection } from '@/components/section/hero-section'
 
 export default function Home() {
   return (
-    <main className="mx-auto flex flex-row flex-wrap items-center justify-center gap-14 rounded-3xl p-14 text-start">
+    <main className="mx-auto flex flex-row flex-wrap items-center justify-center gap-14 rounded-3xl text-start">
       <HeroSection />
       <GallerySection />
     </main>
