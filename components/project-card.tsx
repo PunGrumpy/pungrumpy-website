71d845ae73b0cb4cb6a8cbe7aa4b7f1ca1e5be0d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-04 20:18:31 +0700

    feat: remove following pointer

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -8,7 +8,6 @@ import React from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
-import { FollowerPointerCard } from '@/components/ui/following-pointer'
 import { Separator } from '@/components/ui/separator'
 
 interface ProjectCardProps {
@@ -31,73 +30,45 @@ export function ProjectCard({
   description
 }: ProjectCardProps) {
   return (
-    <FollowerPointerCard
-      title={
-        <TitleComponent
-          title={title}
-          avatar="https://avatars.githubusercontent.com/u/108584943?v=4"
-        />
-      }
+    <motion.div
+      className="flex flex-col"
+      initial={{ opacity: 0, transform: 'translateY(-20px)' }}
+      animate={{ opacity: 1, transform: 'translateY(0)' }}
+      transition={{ duration: 0.5, delay: index * 0.25 }}
     >
-      <motion.div
-        className="flex flex-col"
-        initial={{ opacity: 0, transform: 'translateY(-20px)' }}
-        animate={{ opacity: 1, transform: 'translateY(0)' }}
-        transition={{ duration: 0.5, delay: index * 0.25 }}
-      >
-        <Card className="group bg-card">
-          <CardHeader className="p-2">
-            <div className="relative h-52 w-full">
-              <Image
-                src={imageUrl}
-                alt={title}
-                fill
-                className="rounded-md object-cover transition duration-200 group-hover:scale-95 group-hover:rounded-xl"
-              />
-            </div>
-          </CardHeader>
-          <CardContent className="space-y-4 p-4">
-            <div className="flex items-center gap-2 text-sm text-muted-foreground">
-              {iconType}
-              <span>{projectType}</span>
-            </div>
-            <Separator className="bg-primary/10" />
-            <h3 className="font-medium">{title}</h3>
-            <p className="text-sm text-muted-foreground">{description}</p>
-            <Separator className="bg-primary/10" />
-          </CardContent>
-          <CardFooter className="px-4">
-            <Link href={`/works/${encodeURIComponent(slug)}`}>
-              <Button
-                variant="outline"
-                className="cursor-none rounded-[10px] hover:border-primary/10"
-              >
-                <span>Show more</span>
-                <ChevronRight className="ml-2 size-4" />
-              </Button>
-            </Link>
-          </CardFooter>
-        </Card>
-      </motion.div>
-    </FollowerPointerCard>
+      <Card className="group bg-card">
+        <CardHeader className="p-2">
+          <div className="relative h-52 w-full">
+            <Image
+              src={imageUrl}
+              alt={title}
+              fill
+              className="rounded-md object-cover transition duration-200 group-hover:scale-95 group-hover:rounded-xl"
+            />
+          </div>
+        </CardHeader>
+        <CardContent className="space-y-4 p-4">
+          <div className="flex items-center gap-2 text-sm text-muted-foreground">
+            {iconType}
+            <span>{projectType}</span>
+          </div>
+          <Separator className="bg-primary/10" />
+          <h3 className="font-medium">{title}</h3>
+          <p className="text-sm text-muted-foreground">{description}</p>
+          <Separator className="bg-primary/10" />
+        </CardContent>
+        <CardFooter className="px-4">
+          <Link href={`/works/${encodeURIComponent(slug)}`}>
+            <Button
+              variant="outline"
+              className="rounded-[10px] hover:border-primary/10"
+            >
+              <span>Show more</span>
+              <ChevronRight className="ml-2 size-4" />
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    </motion.div>
   )
 }
-
-const TitleComponent = ({
-  title,
-  avatar
-}: {
-  title: string
-  avatar: string
-}) => (
-  <div className="flex items-center space-x-2">
-    <Image
-      src={avatar}
-      height="20"
-      width="20"
-      alt="Thumbnail"
-      className="rounded-full border border-border object-cover"
-    />
-    <p>{title}</p>
-  </div>
-)
