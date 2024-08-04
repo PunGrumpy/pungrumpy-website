c8a4130e93330502b0ecce21464deead25124ff1
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-04 08:42:46 +0700

    fix: new order image and decrease border color

--- components/section/gallery-section.tsx
+++ components/section/gallery-section.tsx
@@ -34,7 +34,7 @@ export function GallerySection({ className }: GalleryProps) {
 
 export function GalleryImage({ src, alt }: GalleryImageProps) {
   return (
-    <div className="ease-&lsqb;cubic-bezier(0.2,0.8,0.2,1)&rsqb; group relative flex h-80 min-w-[280px] max-w-[520px] flex-1 flex-col items-start justify-start gap-2.5 overflow-hidden rounded-3xl border border-border">
+    <div className="ease-&lsqb;cubic-bezier(0.2,0.8,0.2,1)&rsqb; group relative flex h-80 min-w-72 max-w-lg flex-1 flex-col items-start justify-start overflow-hidden rounded-3xl border border-border/50">
       <Image src={src} alt={alt} fill className="z-0 object-cover" />
     </div>
   )
