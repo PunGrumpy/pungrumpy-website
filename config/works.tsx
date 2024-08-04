79729deb51212ea9e600f58cbac28b5f5f0016f2
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-05 00:05:27 +0700

    feat(works): add 3 works

--- config/works.tsx
+++ config/works.tsx
@@ -50,7 +50,7 @@ export const Works: Work[] = [
       'shadcn/ui',
       'Framer Motion'
     ],
-    roles: ['Creator', 'Frontend Developer', 'UI/UX Designer'],
+    roles: ['Creator', 'Frontend Developer', 'UI/UX Designer', 'Maintainer'],
     links: {
       website: 'https://pongsathorn-portfolio.netlify.app/'
     }
@@ -68,6 +68,56 @@ export const Works: Work[] = [
     links: {
       github: 'https://github.com/PunGrumps/logixlysia/',
       website: 'https://www.npmjs.com/package/logixlysia/'
+    }
+  },
+  {
+    slug: 'kmitl-wizard',
+    imageUrl: '/works/kmitl-wizard.png',
+    iconType: <UsersIcon className="size-4" />,
+    projectType: 'Community Project',
+    title: 'KMITL Wizard',
+    description:
+      "A Chrome extension that transforms KMITL's schedule table into a more beautiful and interactive design with a dark mode.",
+    technologies: ['TypeScript', 'Svelte', 'Tailwind CSS', 'Vite'],
+    roles: ['Creator', 'Frontend Developer', 'Maintainer'],
+    links: {
+      github: 'https://github.com/PunGrumpy/kmitl-wizard/'
+    }
+  },
+  {
+    slug: 'og-tester',
+    imageUrl: '/works/og-tester.png',
+    iconType: <UsersIcon className="size-4" />,
+    projectType: 'Community Project',
+    title: 'OG Tester',
+    description:
+      'A tool to help you debug and preview your metadata (Open Graph, Twitter Card, etc.) before sharing your website on social media.',
+    technologies: [
+      'Next.js',
+      'Tailwind CSS',
+      'TypeScript',
+      'shadcn/ui',
+      'Framer Motion',
+      'Cheerio'
+    ],
+    roles: ['Creator', 'Fullstack Developer', 'Maintainer'],
+    links: {
+      github: 'https://github.com/PunGrumpy/og-tester/',
+      website: 'https://og-tester.pungrumpy.com/'
+    }
+  },
+  {
+    slug: '42-rush-project',
+    imageUrl: '/works/42-rush-project.png',
+    iconType: <GraduationCapIcon className="size-4" />,
+    projectType: 'University Project',
+    title: '42 Rush Project',
+    description:
+      'A simple website that describes members, projects, and activities of the 42 Bangkok Rush team.',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Tailwind CSS'],
+    roles: ['Frontend Developer', 'Maintainer'],
+    links: {
+      website: 'https://42-rush-project.vercel.app/'
     }
   }
 ]
