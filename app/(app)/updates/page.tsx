2904b3ebd75dc34f2f6c71b60742ce21e13de086
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-05 09:17:55 +0700

    feat(page/updates): add metadata (title and description)

--- app/(app)/updates/page.tsx
+++ app/(app)/updates/page.tsx
@@ -1,9 +1,12 @@
-'use client'
-
-import { motion } from 'framer-motion'
+import { Metadata } from 'next'
 
 import { UpdateSection } from '@/components/section/update-section'
 import { Updates } from '@/config/updates'
+
+export const metadata: Metadata = {
+  title: 'Updates',
+  description: 'A describe changelong of my personal website.'
+}
 
 export default function UpdatesPage() {
   return (
@@ -11,20 +14,14 @@ export default function UpdatesPage() {
       {Updates.sort(
         (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
       ).map((update, index) => (
-        <motion.div
+        <UpdateSection
           key={index}
-          initial={{ opacity: 0, transform: 'translateY(-20px)' }}
-          animate={{ opacity: 1, transform: 'translateY(0)' }}
-          transition={{ duration: 0.5, delay: index * 0.25 }}
-        >
-          <UpdateSection
-            key={index}
-            date={update.date}
-            title={update.title}
-            description={update.description}
-            imageSrc={update.image}
-          />
-        </motion.div>
+          index={index}
+          date={update.date}
+          title={update.title}
+          description={update.description}
+          imageSrc={update.image}
+        />
       ))}
     </main>
   )
--- components/section/update-section.tsx
+++ components/section/update-section.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { ChevronRight } from 'lucide-react'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 
-import { Button } from '@/components/ui/button'
-
 interface UpdateItemProps {
+  index: number
   date?: string
   title?: string
   description?: string
@@ -13,29 +12,36 @@ interface UpdateItemProps {
 }
 
 export function UpdateSection({
+  index,
   date,
   title,
   description,
   imageSrc
 }: UpdateItemProps) {
   return (
-    <article className="z-10 flex w-full max-w-6xl flex-wrap items-center gap-8 rounded-xl p-4 text-left text-lg text-muted-foreground sm:gap-14">
-      <div className="flex min-w-full flex-1 flex-col gap-4 sm:min-w-80 sm:gap-8">
-        <div className="w-full text-sm leading-5 sm:text-base">{date}</div>
-        <h2 className="w-full text-3xl font-semibold tracking-tight text-foreground sm:text-5xl">
-          {title}
-        </h2>
-        <p className="w-full text-base leading-7 sm:text-lg">{description}</p>
-      </div>
-      <div className="items-start justify-start overflow-hidden rounded-3xl border border-border transition-transform duration-500 hover:scale-105 sm:hover:scale-110">
-        <Image
-          className="object-cover"
-          src={imageSrc || '/gallery/gallery-1.png'}
-          alt={title || 'Gallery Image'}
-          width={500}
-          height={500}
-        />
-      </div>
-    </article>
+    <motion.div
+      initial={{ opacity: 0, transform: 'translateY(-20px)' }}
+      animate={{ opacity: 1, transform: 'translateY(0)' }}
+      transition={{ duration: 0.5, delay: index * 0.25 }}
+    >
+      <article className="z-10 flex w-full max-w-6xl flex-wrap items-center gap-8 rounded-xl p-4 text-left text-lg text-muted-foreground sm:gap-14">
+        <div className="flex min-w-full flex-1 flex-col gap-4 sm:min-w-80 sm:gap-8">
+          <div className="w-full text-sm leading-5 sm:text-base">{date}</div>
+          <h2 className="w-full text-3xl font-semibold tracking-tight text-foreground sm:text-5xl">
+            {title}
+          </h2>
+          <p className="w-full text-base leading-7 sm:text-lg">{description}</p>
+        </div>
+        <div className="items-start justify-start overflow-hidden rounded-3xl border border-border transition-transform duration-500 hover:scale-105 sm:hover:scale-110">
+          <Image
+            className="object-cover"
+            src={imageSrc || '/gallery/gallery-1.png'}
+            alt={title || 'Gallery Image'}
+            width={500}
+            height={500}
+          />
+        </div>
+      </article>
+    </motion.div>
   )
 }
