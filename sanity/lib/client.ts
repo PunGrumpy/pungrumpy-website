65d5583ee6c5d90091f98a3ce8e0bc800e54e206
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-07 03:02:56 +0700

    chore(sanity/client): disable cache

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -22,7 +22,10 @@ export async function sanityFetcher<QueryResponse>({
   tags: string[]
 }): Promise<QueryResponse> {
   return client.fetch<QueryResponse>(query, qParams, {
-    cache: process.env.NODE_ENV === 'development' ? 'no-store' : 'force-cache',
+    // If you want to enable cache, you can use the following options (for faster loading)
+    // cache: process.env.NODE_ENV === 'development' ? 'no-store' : 'force-cache',
+    // If you want to disable cache, you can use the following options (for latest data)
+    cache: 'no-store',
     next: { tags }
   })
 }
