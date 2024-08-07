4d12e5fdeb1655a0411307539ac6574398ea902d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-07 16:35:23 +0700

    chore(page/global-error): add metadata

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,8 +1,14 @@
 'use client'
 
 import * as Sentry from '@sentry/nextjs'
+import { Metadata } from 'next'
 import NextError from 'next/error'
 import { useEffect } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Error',
+  description: 'An error occurred!'
+}
 
 interface ErrorWithDigest extends Error {
   digest?: string
