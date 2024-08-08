cd1f9dd61921c91d27faa0ce2b6d03b73020ef8f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-08 09:38:27 +0700

    feat(page/takes): add metadata

--- app/(app)/takes/page.tsx
+++ app/(app)/takes/page.tsx
@@ -1,9 +1,18 @@
+import { Metadata } from 'next'
 import React from 'react'
 
 import { TakesGallery } from '@/components/takes/takes-gallery'
 import { sanityFetcher } from '@/sanity/lib/client'
 import { takeQuery } from '@/sanity/lib/query'
 import { TakeInterface } from '@/types'
+
+export const metadata: Metadata = {
+  metadataBase: process.env.NEXT_PUBLIC_METADATA_BASE
+    ? new URL(`${process.env.NEXT_PUBLIC_METADATA_BASE}/takes`)
+    : new URL('http://localhost:3000/takes'),
+  title: 'Takes',
+  description: 'A collection of my takes and photos.'
+}
 
 export default async function TakesPage() {
   const takes: TakeInterface[] = await sanityFetcher({
