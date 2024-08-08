de026c7250c20b65707ea2bcca8710d5ff226fb5
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-08 08:42:57 +0700

    refactor(page/works): change `div`to `main`

--- app/(app)/works/page.tsx
+++ app/(app)/works/page.tsx
@@ -20,24 +20,22 @@ export default async function WorksPage() {
   })
 
   return (
-    <>
-      <div className="gap-15 z-10 mx-auto flex max-w-6xl flex-col items-center">
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          {projects.map((project, index) => (
-            <WorkCard
-              key={project._id}
-              index={index}
-              name={project.name}
-              slug={project.slug}
-              maintainStatus={project.maintainStatus}
-              projectStage={project.projectStage}
-              projectType={project.projectType}
-              tagline={project.tagline}
-              coverImage={project.coverImage.image}
-            />
-          ))}
-        </div>
+    <main className="gap-15 z-10 mx-auto flex max-w-6xl flex-col items-center">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {projects.map((project, index) => (
+          <WorkCard
+            key={project._id}
+            index={index}
+            name={project.name}
+            slug={project.slug}
+            maintainStatus={project.maintainStatus}
+            projectStage={project.projectStage}
+            projectType={project.projectType}
+            tagline={project.tagline}
+            coverImage={project.coverImage.image}
+          />
+        ))}
       </div>
-    </>
+    </main>
   )
 }
