2487e3002cc8df952492ffc2aa1bb4ac39302f9b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-15 09:00:04 +0700

    refactor(projects): update naming

--- app/(app)/projects/page.tsx
+++ app/(app)/projects/page.tsx
@@ -6,18 +6,16 @@ import { fetchProjects } from '@/app/(app)/actions'
 import { SlideInView } from '@/components/animation/slide-in-view'
 import { ProjectCard } from '@/components/card/project-card'
 import { HeadingSection } from '@/components/section/heading-section'
-import { Badge } from '@/components/ui/badge'
-import { Card, CardContent } from '@/components/ui/card'
 
 export const metadata: Metadata = {
   metadataBase: process.env.NEXT_PUBLIC_METADATA_BASE
-    ? new URL(`${process.env.NEXT_PUBLIC_METADATA_BASE}/works`)
-    : new URL('http://localhost:3000/works'),
-  title: 'Works',
+    ? new URL(`${process.env.NEXT_PUBLIC_METADATA_BASE}/projects`)
+    : new URL('http://localhost:3000/projects'),
+  title: 'Projects',
   description: 'A collection of my works and projects.'
 }
 
-export default async function WorksPage() {
+export default async function ProjectsPage() {
   const projects = await fetchProjects()
 
   return (
@@ -34,31 +32,6 @@ export default async function WorksPage() {
               href={`/projects/${encodeURIComponent(project.slug)}`}
               className="flex h-full flex-col"
             >
-              {/* <Card className="relative h-full overflow-hidden bg-card transition-shadow duration-300 hover:border-primary/20">
-                <CardContent className="flex space-x-4 p-4">
-                  <div className="flex size-16 items-center justify-center rounded-md bg-primary/10 text-4xl font-bold">
-                    {project.coverImage ? (
-                      <Image
-                        src={project.coverImage.image}
-                        alt={project.coverImage.alt}
-                        width={64}
-                        height={64}
-                        className="p-2"
-                      />
-                    ) : (
-                      <span>{project.name.charAt(0)}</span>
-                    )}
-                  </div>
-                  <div className="flex flex-1 flex-col justify-between">
-                    <div>
-                      <h3 className="text-lg font-semibold">{project.name}</h3>
-                      <p className="text-sm text-muted-foreground">
-                        {project.repository}
-                      </p>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card> */}
               <ProjectCard project={project} />
             </Link>
           ))}
