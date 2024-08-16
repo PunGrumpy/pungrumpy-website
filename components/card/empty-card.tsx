f537de8efc6fc01b0e23f8fb88c1d0e0502a4aca
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-16 21:51:45 +0700

    refactor(empty-card): add default vaule

--- components/card/empty-card.tsx
+++ components/card/empty-card.tsx
@@ -9,24 +9,21 @@ interface EmptyCardProps {
 }
 
 export const EmptyCard: React.FC<EmptyCardProps> = ({
-  value,
-  icon,
+  value = '',
+  icon = <Icons.logo className="size-16" />,
   title,
   message
 }) => {
+  const defaultTitle = `No ${value} Found`
+  const defaultMessage = `There are no ${value.toLowerCase()} available at this time. Check back again.`
+
   return (
     <Card className="w-full border-dashed">
       <CardContent className="flex flex-col items-center py-8 text-center">
-        <div className="text-4xl">
-          {icon || <Icons.logo className="size-16" />}
-        </div>
-        <h2 className="mb-3">{title ?? `No ${value} Found`}</h2>
+        <div className="text-4xl">{icon}</div>
+        <h2 className="mb-3">{title ?? defaultTitle}</h2>
         <p className="mb-1 max-w-sm text-muted-foreground">
-          {message ??
-            `There are no ${
-              value && value.toLowerCase()
-            } available at this time. Check back
-          again.`}
+          {message ?? defaultMessage}
         </p>
       </CardContent>
     </Card>
