c4a9edb698ed3e5c591fee417095d15b4267ddc2
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-19 22:53:33 +0700

    feat: add preload

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,10 +1,23 @@
 'use client'
 
+import { AnimatePresence } from 'framer-motion'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 import * as React from 'react'
 
+import { Preloader } from './animation/preload'
+
 export function Providers({ children }: ThemeProviderProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(true)
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+      document.body.style.cursor = 'default'
+      window.scrollTo(0, 0)
+    }, 2000)
+  }, [])
+
   return (
     <NextThemesProvider
       attribute="class"
@@ -13,6 +26,9 @@ export function Providers({ children }: ThemeProviderProps) {
       disableTransitionOnChange
     >
       {children}
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
     </NextThemesProvider>
   )
 }
